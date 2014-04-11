defea7571fb5b40fe10093d30eab90d8a9e7d5a8
aaron <asgibson@asu.edu>
2014-04-11 13:53:41 -0700

    Adding Crypto++ CMake file to build Crypto++ during CMake builds.

---
+++ cmake/add_cryptopp.cmake
@@ -0,0 +1,23 @@
+include(ExternalProject)
+ExternalProject_Add(
+    Cryptopp
+    URL http://www.cryptopp.com/cryptopp562.zip
+    URL_HASH SHA1=ddc18ae41c2c940317cd6efe81871686846fa293    
+    TIMEOUT 600
+    #PATCH_COMMAND ${CMAKE_COMMAND} -E copy
+    #    ${CMAKE_SOURCE_DIR}/src/third_party_libs/${BoostFolderName}/boost_variant_detail_move_patched.hpp
+    #    <SOURCE_DIR>/boost/variant/detail/move.hpp
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make
+    BUILD_IN_SOURCE ON
+    INSTALL_COMMAND ""
+    #LOG_DOWNLOAD ON
+    #LOG_UPDATE ON
+    #LOG_CONFIGURE ON
+    #LOG_BUILD ON
+    #LOG_TEST ON
+    #LOG_INSTALL ON
+    )
+
+# Now, set the include and linker paths.
+
